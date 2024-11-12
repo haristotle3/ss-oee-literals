930962ff9c15705e95f8672bf6d5de461699bc83
Harish-Patil-CSE <01FE22BCS173@kletech.ac.in>
2024-11-13 01:01:10 +0530

    Progress in pass-2.c

--- system-software-lab/two-pass-assembler/pass-2.c
+++ system-software-lab/two-pass-assembler/pass-2.c
@@ -4,7 +4,7 @@
 
 #define MAX_TOKEN_LENGTH 25
 
-int passTwo(FILE *, FILE *, FILE *, FILE *, FILE *);
+int passTwo(FILE *, FILE *, FILE *, FILE *, int);
 
 int main()
 {
@@ -14,27 +14,32 @@ int main()
     FILE *symbol_table = fopen("SYMTAB.txt", "r");
     FILE *length = fopen("program_length", "r");
 
+    int code_length;
+    fscanf(length, "%d", &code_length);
+    printf("%d\n", code_length);
+    
     // output files
     FILE *object_program = fopen("object_program.txt", "w");
 
-    passTwo(input_file, operation_code_table, symbol_table, object_program, length);
+    passTwo(input_file, operation_code_table, symbol_table, object_program, code_length);
 
     return 0;
 }
 
-int passTwo(FILE *input_file, FILE *operation_code_table, FILE *symbol_table, FILE *object_program, FILE *length)
+int passTwo(FILE *input_file, FILE *operation_code_table, FILE *symbol_table, FILE *object_program, int code_length)
 {
     char location[MAX_TOKEN_LENGTH];
     char label[MAX_TOKEN_LENGTH];
     char opcode[MAX_TOKEN_LENGTH];
     char operand[MAX_TOKEN_LENGTH];
-    int code_length;
 
     fscanf(input_file, "%s\t%s\t%s", label, opcode, operand);
-    fscanf(length, "%d", &code_length);
+
+    printf("%d\n", code_length);
+    printf("%x\n", code_length);
 
     if (strcmp(opcode, "START") == 0)
     {
-        fprintf(object_program, "H%-6s%06x%06x", label, operand, code_length);
+        fprintf(object_program, "H%-6s%06x%06x\n", label, atoi(operand), code_length);
     }
 }
