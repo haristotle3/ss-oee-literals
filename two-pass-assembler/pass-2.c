4c41ff3b5dda1034019a89dc4404e6a7a776e096
Harish-Patil-CSE <01FE22BCS173@kletech.ac.in>
2024-11-16 14:08:04 +0530

    Implemented all modules except assemble_instruction. Modules need testing as well.

--- two-pass-assembler/pass-2.c
+++ two-pass-assembler/pass-2.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "utils.h"
 
 unsigned int assemble_instruction(opcode, operand, symbol_address);
 // hardest. Need to use some creativity.
-void get_literal_value(operand_without_extraneous, operand);
-// strip the apostropes and the first character 'C' or 'X' and return the remaining string.
-unsigned int get_string_literal_hex(operand);
-// convert string to ascii numbers as an unsigned int and return
-int get_object_code_length(assembled_object_code);
-// get the length of the code, just take log to the base 16 and ceil func it.
-void update_text_record_length(temp_text_record, text_record_length);
-// self explanatory
+void get_literal_value(char operand_without_extraneous[], char operand[]);
+unsigned long int get_string_literal_hex(char operand_without_extraneous[]);
+int get_object_code_length(unsigned long int assembled_object_code);
+void update_text_record_length(FILE *temp_text_record, int text_record_length);
 
 void increment_pc();
 void init_pc_file();
@@ -116,7 +113,7 @@ int passTwo(FILE *input_file, FILE *object_program, FILE *assembly_listing)
             if (operand[0] == 'X')
                 assembled_object_code = strtol(operand_without_extraneous, NULL, 16);
             else if (operand[0] == 'C')
-                assembled_object_code = get_string_literal_hex(operand);
+                assembled_object_code = get_string_literal_hex(operand_without_extraneous);
             else
             {
                 printf("ERROR: Unknown literal %s\n", operand);
@@ -153,7 +150,7 @@ int passTwo(FILE *input_file, FILE *object_program, FILE *assembly_listing)
     fclose(temp_text_record);
 
     // Write the end record
-    fprintf(object_program, "E%x\n", start_address);
+    fprintf(object_program, "E%06x\n", start_address);
     fclose(PROGRAM_COUNTER_FILE);
     return 1;
 }
@@ -179,7 +176,50 @@ void init_pc_file()
 void increment_pc()
 {
     if (!feof(PROGRAM_COUNTER_FILE))
-        fscanf(PROGRAM_COUNTER_FILE, "%x\t%*s\t%*s\t%*s", &PROGRAM_COUNTER);
+        fscanf(PROGRAM_COUNTER_FILE, "%x\t%*s\t%*s\t%*s\n", &PROGRAM_COUNTER);
+
+    return;
+}
+
+int get_object_code_length(unsigned long int assembled_object_code)
+{
+    // get the length of the code,
+    // just take log to the base 16 and apply ceiling funcition to it.
+
+    // By change of base formula,
+    // log16(x) = log2(x)/log2(16) = log2(x) / 4
+
+    return ceil(log2(assembled_object_code) / 4);
+}
+
+unsigned long int get_string_literal_hex(char operand_without_extraneous[])
+{
+    // convert string to ascii value string (as an unsigned long int) and return.
+    unsigned long int obj_code = 0;
+
+    for (int i = 0; i < strlen(operand_without_extraneous); i++)
+    {
+        obj_code <<= 8;
+        obj_code += operand_without_extraneous[i];
+    }
+
+    return obj_code;
+}
+
+void get_literal_value(char operand_without_extraneous[], char operand[])
+{
+    // strip the apostropes and the first character
+    // 'C' or 'X' and return the remaining string.
+    // We use strncpy.
+
+    strncpy(operand_without_extraneous, operand + 2, strlen(operand) - 3);
+    return;
+}
+
+void update_text_record_length(FILE *temp_text_record, int text_record_length)
+{
+    fseek(temp_text_record, 1 + 6, SEEK_SET);
+    fprintf(temp_text_record, "%02x", text_record_length);
 
     return;
 }
