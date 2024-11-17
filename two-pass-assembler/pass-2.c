8baac8f4280731f635a1d5228c495e48cf9f30cb
Harish-Patil-CSE <01FE22BCS173@kletech.ac.in>
2024-11-17 19:53:53 +0530

    Removed extra files. Will run a possibly final test now.

--- two-pass-assembler/pass-2.c
+++ two-pass-assembler/pass-2.c
@@ -211,18 +211,17 @@ unsigned long long int assemble_instruction(char mnemonic[], char operand[], int
     else if (instruction_format == 3 || instruction_format == 4)
     {
         assembled_object_code = opcode_value(mnemonic);
-        assembled_object_code <<= 2;
         // Check if indirect
         if (operand[0] == '@')
             // Set n flag on;
             assembled_object_code += 2;
+        // Check if immediate
         else if (operand[0] == '#')
             // Set i flag on;
             assembled_object_code += 1;
         else
             // neither immediate nor indirect
             assembled_object_code += 3;
-
         // Dealing with x, b, p, e flags
         // We don't need any other registers as variables other than program counter and base
 
@@ -233,12 +232,20 @@ unsigned long long int assemble_instruction(char mnemonic[], char operand[], int
 
         // B and P flags
         assembled_object_code <<= 2;
-        int displacement = symbol_address - PROGRAM_COUNTER;
+        int displacement;
 
-        if (-2048 <= displacement && displacement <= +2047)
+        if (operand[0] == '#')
+            displacement = symbol_address;
+        else
+            displacement = symbol_address - PROGRAM_COUNTER;
+
+        if (operand[0] != '#' && -2048 <= displacement && displacement <= +2047)
             assembled_object_code += 1; // Set P flag to 1;
-        else if (0 <= displacement && displacement <= 4095)
+        else if (operand[0] != '#' && 0 <= displacement && displacement <= 4095)
             assembled_object_code += 2; // Set B flag to 1;
+        else // value too big to be addressed from base relative or PC relative.
+        // It will be a format 4 instruction.
+            displacement = symbol_address;
 
         // Set E flag
         assembled_object_code <<= 1;
@@ -247,9 +254,15 @@ unsigned long long int assemble_instruction(char mnemonic[], char operand[], int
 
         // Add displacement field.
         if (instruction_format == 3)
+        {
+            displacement &= 0x00000fff;
             assembled_object_code <<= 12;
+        }
         else if (instruction_format == 4)
+        {
+            displacement &= 0x000fffff;
             assembled_object_code <<= 20;
+        }
 
         assembled_object_code += displacement;
     }
