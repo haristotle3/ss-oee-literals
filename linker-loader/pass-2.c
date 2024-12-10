e7be4b4095f4e93556d9f12c51c653bd253a3f6b
haristotle3 <haristotlepatil2003@gmail.com>
2024-12-10 23:46:10 +0530

    Pass 1 error handing improved

--- linker-loader/pass-2.c
+++ linker-loader/pass-2.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define ERROR_VALUE -1;
 
-int main() {
-    
+int main()
+{
+    FILE *object_programs = fopen("object_programs.txt", "r");
+    FILE *load_address_file = fopen("load_address.txt", "r");
+
+    int PROGADDR;
+    fscanf(load_address_file, "%x", &PROGADDR);
+
+    if (ll_pass_two(object_programs, PROGADDR) == ERROR_VALUE)
+        printf("Loading failed.\n");
+    else
+        printf("Success!\n");
 }
