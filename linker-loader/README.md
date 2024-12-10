7e98d2f33a5eac6c97fcd46b64b2ce50b6022583
haristotle3 <haristotlepatil2003@gmail.com>
2024-12-11 00:24:35 +0530

    Added readme and progressed with pass-2

---
+++ linker-loader/README.md
@@ -0,0 +1,9 @@
+## To run pass one in one command:
+
+`gcc -o pass-1.exe pass-1.c && ./pass-1.exe`
+
+## To run pass 2 in one command:
+
+`gcc -o pass-2.exe pass-2.c && ./pass-2.exe`
+
+# Object code instructions in `object_programs.txt` need to be seperated by spaces else we cannot differentiate between 2 instructions.
--- linker-loader/pass-2.c
+++ linker-loader/pass-2.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define ERROR_VALUE -1;
+#define ERROR_VALUE -1
+#define MAX_BUF 512
+#define WORD_SIZE 6
+
+int ll_pass_two(FILE *object_programs, int PROGADDR);
+void move_objcode_to_memory(FILE *object_programs, int tr_start_address, int tr_length, int CSLTH);
 
 int main()
 {
@@ -16,3 +21,45 @@ int main()
     else
         printf("Success!\n");
 }
+
+int ll_pass_two(FILE *object_programs, int PROGADDR)
+{
+    int CSADDR = PROGADDR;
+    int EXECADDR = PROGADDR;
+
+    typedef struct
+    {
+        char record_type;
+        char csect_name[MAX_BUF];
+        int csect_address;
+        int CSLTH;
+    } header_record;
+
+    typedef struct
+    {
+        int start_address;
+        short int length;
+        int object_code;
+    } text_record;
+
+    header_record hr;
+    text_record tr;
+    char current_record_type;
+
+    while (fscanf(object_programs, "%c%6s%6x%6x\n", &hr.record_type, hr.csect_name, &hr.csect_address, &hr.CSLTH) > 0)
+    {
+        current_record_type = hr.record_type;
+        while (current_record_type != 'E')
+        {
+            fscanf(object_programs, "%c", &current_record_type);
+            if (current_record_type == 'T')
+            {
+                fscanf(object_programs, "%6x%2x", &tr.start_address, &tr.length);
+                move_objcode_to_memory(object_programs, tr.start_address, tr.length, hr.CSLTH);
+            }
+        }
+    }
+    return 1;
+}
+
+void move_objcode_to_memory(FILE *object_programs, int tr_start_address, int tr_length, int CSLTH);
